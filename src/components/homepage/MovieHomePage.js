05334fc6af71708e63c308d5402cd1ed9027b705
Ian Kincaid <ikbusinessacct@gmail.com>
2020-05-06 01:55:49 -0400

    base template extends and a few test div's

---
+++ src/components/homepage/MovieHomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+export default class MovieHomePage extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        )
+    }
+}
