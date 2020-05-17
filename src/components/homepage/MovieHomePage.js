c1e04e5cee9587468a7b901f81e3dfcfdf08f996
Ian Kincaid <ikbusinessacct@gmail.com>
2020-05-17 00:05:02 -0400

    downloaded react-router-dom now playing with the layout

--- src/components/homepage/MovieHomePage.js
+++ src/components/homepage/MovieHomePage.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+import './moviehomepage.css'
 
 export default class MovieHomePage extends Component {
     render() {
         return (
             <div>
+                <ul>
+                    <li>Home</li>
+                    <li>New Movies</li>
+                    <li>Catagories</li>
+                    <li>Settings</li>
+                </ul>
                 <div>
-                    <div></div>
+                    <div>
+                        <img></img>
+                    </div>
                 </div>
             </div>
         )
