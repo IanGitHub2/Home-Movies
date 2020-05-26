43d3a76c4d5d5038e2e9bdceba1010ccefd9d26f
Ian Kincaid <ikbusinessacct@gmail.com>
2020-05-26 01:56:26 -0400

    css changes still working on layout but have and idea now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import MovieHomePage from './components/homepage/MovieHomePage'
 import './App.css';
 
--- src/components/homepage/MovieHomePage.js
+++ src/components/homepage/MovieHomePage.js
@@ -6,17 +6,34 @@ export default class MovieHomePage extends Component {
     render() {
         return (
             <div>
-                <ul>
-                    <li>Home</li>
-                    <li>New Movies</li>
-                    <li>Catagories</li>
-                    <li>Settings</li>
-                </ul>
-                <div>
+                <nav>
+                    <ul class="homenav">
+                        <li><a herf="#"> Profile </a></li>
+                        <li><a herf="#"> New Movies </a></li>
+                        <li><a herf="#"> Catagories </a></li>
+                        <li><a herf="#"> Setting </a></li>
+                    </ul>
+                </nav>
+                <trailer>
                     <div>
-                        <img></img>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS8t1NO-N4rFKnmw-8AdUJdwrWuJJIwOGEzoQJc0rLY7oP8oPtG&usqp=CAU" class="hometrailer" alt="Movie Trailer"></img>
                     </div>
-                </div>
+                </trailer>
+                <selection>
+                    {/* swicth to links */}
+                    <div class="homeselection">
+                        <div>New Movies</div>
+                    </div>
+                    <div class="homeselection">
+                        <div>Action</div>
+                    </div>
+                    <div class="homeselection">
+                        <div>Comedy</div>
+                    </div>
+                    <div class="homeselection">
+                        <div>Science Fiction</div>
+                    </div>
+                </selection>
             </div>
         )
     }
