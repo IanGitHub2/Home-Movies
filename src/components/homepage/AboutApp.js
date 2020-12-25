202e0281ee9b7f93fdbe0003b655476fb126c52c
Ian Kincaid <ikbusinessacct@gmail.com>
2020-12-25 02:53:30 -0500

    about me logo a description for api that i want to use

--- src/components/homepage/AboutApp.js
+++ src/components/homepage/AboutApp.js
@@ -7,8 +7,14 @@ export default class AboutApp extends React.Component {
         return(
             <div>
                 <TopNavBar/>
-                <div>Block 1</div>
-                <div>Block 2</div>
+                <div>
+                    <div>About this app.</div>
+                </div>
+                <div>
+                    <img alt='The Movie DB Logo' src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg'/>
+                    <div>"This product uses the TMDb API but is not endorsed or certified by TMDb."</div>
+                    <div><a href='https://www.themoviedb.org/'>Link to The Movie DB</a></div>
+                </div>
                 <div>Block 3</div>
                 <div>Block 4</div>
                 <BottomNavBar/>
