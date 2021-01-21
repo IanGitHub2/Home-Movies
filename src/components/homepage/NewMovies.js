03545adf6ea6e7ac8c5f555bc579033ad85d66b4
Ian Kincaid <ikbusinessacct@gmail.com>
2021-01-20 19:47:47 -0500

    css for middle section movies

--- src/components/homepage/NewMovies.js
+++ src/components/homepage/NewMovies.js
@@ -5,19 +5,21 @@ export default class NewMovies extends React.Component {
   
     render(){
         return(
-            <div className="movie-container">
-              <div className="new-movie-genre-title">New Movies</div>
-              {this.props.newMovies.map((movie, i) => { 
-                return(
-                  <div className="each_movie">
-                      <div key={movie.id}>
-                        <img src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`} alt="Movie cover art"/>
-                        <div>{ movie.title}</div>
-                      </div>
-                  </div>
-                )
-              })}
+          <div>
+            <div className="new-movie-genre-title">New Movies</div>
+            <div id="movie-container" className="movie-middle-section-container">
+                {this.props.newMovies.map((movie, i) => { 
+                  return(
+                    <div className="each_movie">
+                        <div key={movie.id}>
+                          <img src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`} alt="Movie cover art"/>
+                          <div>{ movie.title}</div>
+                        </div>
+                    </div>
+                    )
+                })}
 
+              </div>
             </div>
         )
     }
--- src/components/homepage/Trending.js
+++ src/components/homepage/Trending.js
@@ -5,18 +5,20 @@ import './trending.css'
 export default class Trending extends React.Component {
     render() {
         return(
-            <div className='trending-container'>
+            <div>
                 <div className="new-movie-genre-title">Trending Movies</div>
-                {this.props.popularMovies.map((movie, i) => {
-                    return(
-                        <div className="each_movie">
-                            <div>
-                                <img src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`} alt="Movie cover art"/>
-                                <div>{movie.title}</div>
+                <div id='trending-container' className="movie-middle-section-container">
+                    {this.props.popularMovies.map((movie, i) => {
+                        return(
+                            <div className="each_movie">
+                                <div>
+                                    <img src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`} alt="Movie cover art"/>
+                                    <div>{movie.title}</div>
+                                </div>
                             </div>
-                        </div>
-                    )
-                 })}
+                        )
+                    })}
+                </div>
             </div>
         )
     }
