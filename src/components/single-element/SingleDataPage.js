7861be9e4f74464fd28fd9158847e10c548b3862
Ian Kincaid <ikbusinessacct@gmail.com>
2021-02-12 19:50:33 -0500

    single post css touch up

--- src/components/single-element/SingleDataPage.js
+++ src/components/single-element/SingleDataPage.js
@@ -8,41 +8,46 @@ export default class SingleDataPage extends React.Component {
             <div id='pagebody'>
                 {this.props.singlePostElement.map((single, i ) => {
                     return(
-                        <div key={single.id}>
-                            <div >
-                                <div className="line-up">
-                                    <p className="bold-tag"> Tagline: </p> 
-                                    <p className="regualer-tag"> {single.tagline} </p>
-                                </div>
+                        <div id="single-container" key={single.id}>
+                            <div id="single-tagline" className="line-up">
+                                <p className="bold-tag"> Tagline: </p> 
+                                <p className="regualer-tag"> {single.tagline} </p>
+                            </div>
 
+                            <div id="single-img">
                                 <img src={`https://image.tmdb.org/t/p/w185${single.poster_path}`} alt="Movie cover art" id="img-tag"/>
+                            </div>
 
+                            <div id="single-overview">
+                                <p className="regualer-tag">Overview: {single.overview}</p>
+                            </div>
+
+                            <div id="single-title">
                                 <div className="bold-tag">{ single.title }</div>
                             </div>
-                            <div>
-                                <div className="line-up">
-                                    <p className="bold-tag">Realease date:</p> 
-                                    <p className="regualer-tag">{single.release_date}</p>
-                                </div>
+                            
+                            <div id="single-release" className="line-up">
+                                <p className="bold-tag">Realease date:</p> 
+                                <p className="regualer-tag">{single.release_date}</p>
+                            </div>
 
-                                <div className="line-up">
-                                    <p className="bold-tag">Movie Status:</p> 
-                                    <p className="regualer-tag">{single.status}</p>
-                                </div>
+                            <div id="single-status" className="line-up">
+                                <p className="bold-tag">Movie Status:</p> 
+                                <p className="regualer-tag">{single.status}</p>
+                            </div>
 
-                                <div className="line-up">
-                                    <p className="bold-tag">Length:</p> 
-                                    <p className="regualer-tag">{single.runtime}</p>
-                                </div>
+                            <div id="single-length" className="line-up">
+                                <p className="bold-tag">Length:</p> 
+                                <p className="regualer-tag">{single.runtime}</p>
+                            </div>
 
+                            <div id="single-genres">
                                 <div>Genres: </div>
                             </div>
-                            <div>
-                                <div>Budget: ${single.budget}</div>
-                                <div>Revenue: ${single.revenue}</div>
-                            </div>
-                            <div>
-                                <p className="regualer-tag">Overview: {single.overview}</p>
+
+                            <div id="single-money" className="sin-money">
+                                <p className="bold-tag">Budget: $</p><p className="regualer-tag">{single.budget}</p>
+                                <p className="bold-tag">Revenue: $</p><p className="regualer-tag">{single.revenue}</p>
                             </div>
                         </div>
                     )
