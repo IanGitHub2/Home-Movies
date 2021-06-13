0331cb08a35ba5ac7914ef3033a4c553f03fcb9d
Ian Kincaid <ikbusinessacct@gmail.com>
2021-06-13 03:53:57 -0400

    updated model's file with function code

--- model/movies.js
+++ model/movies.js
@@ -1,8 +1,38 @@
-const mongoose = require('./connection.js')
+const mongoose = require('./connection.js');
 const Schema = mongoose.Schema;
 
 const MoviesSchema = new Schema({
-    like: String,
-})
+    Id: String,
+    like: Boolean,
+    Watch: Boolean,
+});
 
 const MoviesCollection = mongoose.model('Movie', MoviesSchema);
+
+const grabAllData = () => {
+    return MoviesCollection.find()
+};
+
+const grabSingleData = (dataId) => {
+    return MoviesCollection.findById(dataId)
+};
+
+const addData = (dataId) => {
+    return MoviesCollection.create(dataId)
+};
+
+const updateData = (dataId, updateData) => {
+    return MoviesCollection.findByIdAndUpdate(dataId, updateData)
+};
+
+const deleteData = (dataId) => {
+    return MoviesCollection.deleteOne({_id: dataId})
+};
+
+module.export = {
+    grabAllData,
+    grabSingleData,
+    addData,
+    updateData,
+    deleteData
+};
