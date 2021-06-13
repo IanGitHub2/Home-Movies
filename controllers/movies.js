0594357fed3c8440e89ff665b48100ce2782bd6a
Ian Kincaid <ikbusinessacct@gmail.com>
2021-06-13 04:01:16 -0400

    Renamed model file to models and added code to controllers

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const moviesApi = require('../model/movies.js')
+const moviesApi = require('../models/movies.js')
 const moviesRouter = express.Router()
 
-
+moviesRouter.get('/', (req, res) => {
+    moviesApi.grabAllData()
+        .then((allData) => {
+            res.json(allData)
+        })
+});
 
 module.exports = {
     moviesRouter
