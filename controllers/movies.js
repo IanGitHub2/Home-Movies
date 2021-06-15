955af01771227987b2991d586bb4f9365e03d04b
Ian Kincaid <ikbusinessacct@gmail.com>
2021-06-15 00:53:03 -0400

    added the rest of the code for controllers file

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,6 +9,34 @@ moviesRouter.get('/', (req, res) => {
         })
 });
 
+moviesRouter.post('/', (req, res) => {
+    moviesApi.addData(req.body)
+        .then((data) => {
+            res.json({data})
+        })
+});
+
+moviesRouter.get('/:dataId', (req, res) => {
+    moviesApi.grabSingleData(req.params.dataId)
+        .then((data) => {
+            res.json(data)
+        })
+});
+
+moviesRouter.put('/:dataId', (req, res) => {
+    moviesApi.updateData(req.params.dataId, req.body)
+        .then(() => {
+            res.json({})
+        })
+});
+
+moviesRouter.delete('/:dataId', (req, res) => {
+    moviesApi.deleteData(req.params.dataId)
+        .then(() => {
+            res.json({})
+        })
+});
+
 module.exports = {
     moviesRouter
-}
+};
