384669fbd39518fb5bff3e600b7fc241d16c0868
Ian Kincaid <ikbusinessacct@gmail.com>
2021-06-20 14:34:54 -0400

    still moving things around this currently does not start

---
+++ client/build/precache-manifest.fa21b77aadaea7342978d027d84ca366.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79cb5cb6b21cea271cd4361813e37a28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "336ea9a412d80d1c88a4",
+    "url": "/static/css/main.95dba09d.chunk.css"
+  },
+  {
+    "revision": "7347cb6975f2420039a0",
+    "url": "/static/js/2.249d3d40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.249d3d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336ea9a412d80d1c88a4",
+    "url": "/static/js/main.8f5bfa06.chunk.js"
+  },
+  {
+    "revision": "b6934ce7a45dbd89b840",
+    "url": "/static/js/runtime-main.eda4a3ba.js"
+  }
+]);
