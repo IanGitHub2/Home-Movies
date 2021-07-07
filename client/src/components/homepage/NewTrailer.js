2a8b821f20c3d1d0942502252dda7387af6697d2
Ian Kincaid <ikbusinessacct@gmail.com>
2021-07-07 18:39:38 -0400

    css changes for better phone optamization 2

--- client/src/components/homepage/NewTrailer.js
+++ client/src/components/homepage/NewTrailer.js
@@ -6,7 +6,7 @@ export default class NewTrailer extends React.Component {
             <div>
                 <div>
                     <div>
-                      <iframe width="966" height="543" src="https://www.youtube.com/embed/ZnKvQbpDYXU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="scoob"></iframe>
+                      <iframe className="hometrailer" src="https://www.youtube.com/embed/ZnKvQbpDYXU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="scoob"></iframe>
                     </div>
                 </div>
             </div>
